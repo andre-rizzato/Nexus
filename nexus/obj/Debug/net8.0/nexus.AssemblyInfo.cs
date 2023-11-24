df1fec0cea749aab462a655acc210ae26712c3a6
Andre Rizzato <andre.rizzato@outlook.com>
2023-11-24 20:55:47 -0300

    windows user change

--- nexus/obj/Debug/net8.0/nexus.AssemblyInfo.cs
+++ nexus/obj/Debug/net8.0/nexus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nexus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f886f24cd550ed157d98424be9071513f45c21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("nexus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nexus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
